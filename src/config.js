f3ea8719b438dc8daf498b7ae040d97e964e0bcd
wave <986887991@qq.com>
2019-06-25 16:20:20 +0800

    api 802

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 //开发模式
 export const IS_DEV = process.env.NODE_ENV !== 'production';
 //正式、测试网络的api
-export let API_URL = 'http://wx.inchain.org';
+export let API_URL = 'http://wx.inchain.org:802';
 //请求最迟时间
 export const API_TIME = IS_DEV ? '9000' : '8000';
 
